1f5b959e3c1cfbe0b305d4d5e50de4f286899b88
Daniele Gioria <daniele1gioria@gmail.com>
2021-04-30 14:59:21 +0200

    Added bootstrap package

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,12 +8,6 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="google" content="notranslate" />
           <meta name="theme-color" content="#1976D2" />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-            crossOrigin="anonymous"
-          />
         </Head>
         <body>
           <Main />
