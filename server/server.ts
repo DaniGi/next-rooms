ca842f8ed8897dbebe9023bcc46c4e7e48de6a7d
Daniele Gioria <daniele1gioria@gmail.com>
2021-05-05 14:44:33 +0200

    Added mongoDB connection

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,20 @@ import express, { Express, Request, Response } from 'express';
 import * as http from 'http';
 import next, { NextApiHandler } from 'next';
 import * as socketio from 'socket.io';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `${__dirname}/../.env.development.local` });
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER_EDIT_ONLY}:${process.env.MONGO_PASSWORD_EDIT_ONLY}@cluster0.y8qeq.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  )
+  .then(() => console.log(`> Connected to MongoDB database: ${process.env.MONGO_DB_NAME}`))
+  .catch(() =>
+    console.log(`> Failed connection to MongoDB database: ${process.env.MONGO_DB_NAME}`),
+  );
 
 const PORT: number = parseInt(process.env.PORT || '3000', 10);
 const dev: boolean = process.env.NODE_ENV !== 'production';
