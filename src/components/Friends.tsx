7d918787ae6470c23ec0978139fb3b3875d0f95e
Daniele Gioria <daniele1gioria@gmail.com>
2021-05-07 12:10:34 +0200

    Added friend list not hard-coded

--- src/components/Friends.tsx
+++ src/components/Friends.tsx
@@ -1,44 +1,19 @@
 import * as React from 'react';
 import styles from './Sidebar.module.css';
 
+import { useUser } from '../contexts/UserContext';
+
 interface Props {}
 
-const friends = [
-  { name: 'Johanna', isConnected: false },
-  { name: 'Edd', isConnected: true },
-  { name: 'Anna', isConnected: true },
-  { name: 'Olga', isConnected: false },
-  { name: 'Jo', isConnected: false },
-  { name: 'Edd', isConnected: true },
-  { name: 'Anna', isConnected: true },
-  { name: 'Olga', isConnected: false },
-  { name: 'Jo', isConnected: false },
-  { name: 'Edd', isConnected: true },
-  { name: 'Anna', isConnected: true },
-  { name: 'Olga', isConnected: false },
-  { name: 'Jo', isConnected: false },
-  { name: 'Edd', isConnected: true },
-  { name: 'Anna', isConnected: true },
-  { name: 'Olga', isConnected: false },
-  { name: 'Jo', isConnected: false },
-  { name: 'Edd', isConnected: true },
-  { name: 'Anna', isConnected: true },
-  { name: 'Olga', isConnected: false },
-  { name: 'Jo', isConnected: false },
-  { name: 'Edd', isConnected: true },
-  { name: 'Anna', isConnected: true },
-  { name: 'Olga', isConnected: false },
-];
-
 const Friends: React.FC<Props> = () => {
+  const { userState } = useUser();
   return (
     <>
-      {friends.map((friend) => (
+      {userState.friends.map((username) => (
         <div
           className={`d-flex flex-row align-items-center justify-content-between p-2 ${styles.sidebarListElement}`}
         >
-          <div>{friend.name}</div>
-          <div>{friend.isConnected && 'Connected'}</div>
+          <div>{username}</div>
         </div>
       ))}
     </>
